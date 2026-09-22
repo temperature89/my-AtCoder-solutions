61c0bef582c4b36146a08ae301eb42f6720bcaeb
temperature89 <akirock689@gmail.com>
2026-09-22 22:09:18 +0900

    Implement base conversion functions and refactor solve logic for problem 067

--- typical90/067.py
+++ typical90/067.py
@@ -1,4 +1,35 @@
-N, K = list(map(int, input().split()))
-for k in range(K):
-    N = N - N // 8
-    print(N)
+def base8_to_base10(s):
+    val = 0
+    for char in s:
+        digit = ord(char) - ord('0')
+        val = val * 8 + digit
+    return val
+
+def base10_to_base9(n):
+    if n == 0:
+        return "0"
+    
+    res = []
+    while n > 0:
+        res.append(str(n % 9))
+        n //= 9
+    
+    return "".join(reversed(res))
+
+def solve():
+    N, K = input().split()
+    K = int(K)
+
+    if N == "0":
+        print(0)
+        return
+    
+    curr = N
+    for _ in range(K):
+        val_10 = base8_to_base10(curr)
+        s_9 = base10_to_base9(val_10)
+        curr = s_9.replace('8', '5')
+    print(curr)
+
+if __name__ == "__main__":
+    solve()
